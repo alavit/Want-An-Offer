31d2955a28f3995a30720ae53d20543b6c9d259c
Aleksei Avitsuk <aleksei.avitsuk@gmail.com>
2025-09-07 16:12:07 +0200

    fix typo

--- src/practice_8/part_2/Task4.java
+++ src/practice_8/part_2/Task4.java
@@ -7,9 +7,9 @@ import java.util.stream.Collectors;
 public class Task4 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        List<Integer> sqaures = numbers.stream()
+        List<Integer> squares = numbers.stream()
                 .map(n -> n * n)
                 .collect(Collectors.toList());
-        System.out.println(sqaures);
+        System.out.println(squares);
     }
 }
